dd1fa3f0b1a21c8f8051a6552bc81e66f7ae250c
Romzik Peperomzik <warriorway5657@gmail.com>
2021-05-28 22:13:29 +0300

    + db папка, там будут клиент+номер. Начальные попытки написать сервер

--- server.py
+++ server.py
@@ -1,27 +1,88 @@
 import socket
 from enum import Enum
 
-retranslated_verb = {"ОТДОВАЙ": 'GET',
-                    "УДОЛИ": 'DELETE',
-                    "ЗОПИШИ": 'WRITE'}
+
+class CanNotParseResponseError(Exception):
+    """ Error that occurs when we can not parse some strange
+        response from RKSOK client."""
+    pass
+
+
+ru_verbs_list = {"ОТДОВАЙ": 'GET',
+                 "УДОЛИ": 'DELETE',
+                 "ЗОПИШИ": 'WRITE'}
+
+PROTOCOL = "РКСОК/1.0"
+ENCODING = "UTF-8"  
+
+def parse_response(conn: str) -> str:
+    """ Processing client response
+        Args:
+            raw_response (str): Запрос.
+        Raises:
+            CanNotParseResponseError: Ошибка если команду запроса неудалось обработать.
+        Returns:
+            str: [description]"""    
+    data = f'{conn.recv(1024).decode(ENCODING)}'
+    for ru_verb in ru_verbs_list:
+        if data.startswith(ru_verb):
+            break
+    else:
+        raise CanNotParseResponseError()  
+
+    validation_request = get_validation_body(data)
+    return send_valid_request(validation_request)
     
 
-server = socket.create_server(("0.0.0.0", 3333))  # Принимаем любые запросы этой машины на порт 3333.
-server.listen(1)  # Длина очереди.
+def get_validation_body(decoded_data: str) -> bytes:
+    """ Composes validation request.
+        Args:
+            decoded_data (str): [description]
+        Returns:
+            bytes: Encoded request"""
+    request = f"АМОЖНА? {PROTOCOL}\r\n{decoded_data}"
+    request += "\r\n"
+    return request.encode(ENCODING)
 
-while True:
+
+def send_valid_request(request_body: bytes) -> str:
+    """ Send validation request and receive valid response.
+        Args:
+            request_body (bytes): Body request.
+        Returns:
+            str: Decoded valid response."""
+    valid_conn = socket.create_connection(('vragi-vezde.to.digital', 51624))
+    valid_conn.sendall(request_body)
+    valid_response = receive_response_body(valid_conn)
+    return valid_response  
+
+
+def receive_response_body(valid_conn) -> str:
+        """ Receives data from socket connection and returns it as string,
+            decoded using ENCODING"""
+        response = b""
+        while True:
+            data = valid_conn.recv(1024)
+            if not data: break
+            response += data
+        return response.decode(ENCODING)  # Возвращаем декодированный ответ от сервера.
+
+def run_server() -> None:
+    """Waiting for a client and proceed client request."""
+    server = socket.create_server(("0.0.0.0", 3333))  # Принимаем любые запросы этой машины на порт 3333.
+    server.listen(1)  # Длина очереди.
     print('Waiting for connection...')
     conn, addr = server.accept()  # Объект для работы с клиентским сокетом. Адрес клиента.
     print('Got new connection')
-    while True:
-        data = f'{conn.recv(1024).decode("UTF-8")}'
-        for verb in retranslated_verb:
-            if data.startswith(verb):
-                mode_to_verb = retranslated_verb[verb]
-                break
-        print(data)
-        break
-    response = f'НОРМАЛДЫКС РКСОК/1.0\r\n812334554\r\n{mode_to_verb}\r\n\r\n'
-    conn.send(response.encode("UTF-8"))
-    conn.shutdown(socket.SHUT_RDWR)
+    valid_or_not_response = parse_response(conn)
+
+    if valid_or_not_response.startswith('МОЖНА'):
+        # Опиши логику если можно обработать
+        response = f'НОРМАЛДЫКС РКСОК/1.0\r\n812334554\r\n\r\n'
+        conn.send(response.encode(ENCODING))
+        conn.shutdown(socket.SHUT_RDWR)
+    else:
+        # А здесь если нельзя
+        pass
+    
 # server.close()
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
+import base64
 
+# Иваннн Хмурый Дацузбе Акпку
 raw_response = 'НОРМАЛДЫКС РКСОК/1.0\r\n89012345678' 
-foo = 'ЗОПИШИ Иван Хмурый РКСОК/1.0\r\n89012345678 — мобильный\r\n02 — рабочий\r\n'
-
-# with open(f'')
+foo = 'ЗОПИШИ SDferwwe erGrgrg РКСОК/1.0\r\n89012345678 — мобильный\r\n02 — рабочий\r\n'
+name = foo.split('\r\n', 1)[0].rsplit(' ', 1)[0].split(' ', 1)[1]
+# print(name)
+encode_name = base64.b64encode(name.encode("UTF-8")).decode()
+print(encode_name)
+with open(f"{encode_name}", 'x', encoding='utf-8') as f:
+    f.write(foo.split('\r\n', 1)[1])
